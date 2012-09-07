461403a28f2ba85ee74641cffc0ef6a7bf20c3aa
Michael Hellein <michael@bearded.com>
2012-09-07 14:18:24 -0400

    Added stub classes to allow ActiveRecord serialization to succeed.

--- lib/redmine_client/issue.rb
+++ lib/redmine_client/issue.rb
@@ -1,4 +1,18 @@
 module RedmineClient
   class Issue < RedmineClient::Base
+    class Status < RedmineClient::Base
+    end
+    class Tracker < RedmineClient::Base
+    end
+    class AssignedTo < RedmineClient::Base
+    end
+    class Parent < RedmineClient::Base
+    end
+    class Author < RedmineClient::Base
+    end
+    class Priority < RedmineClient::Base
+    end
+    class FixedVersion < RedmineClient::Base
+    end
   end
 end
